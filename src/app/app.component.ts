02f4d26cfc2252ed490028338aff1afc85b4f735
mua-wallace <muawallizewi@gmail.com>
2020-03-11 15:39:19 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  title = 'Todo-app';
+  todos = [ {
+    label: 'teke rene',
+    done: false,
+    priority:3
+  },
+  {
+    label: 'Doing Laundry',
+    done: true,
+    priority:1
+  },
+  {
+    label: 'Paying the electric bill',
+    done: false,
+    priority:2
+  },
+  {
+    label: 'Attending a class on php',
+    done: false,
+    priority:4
+  }
+];
+addTodo(newTodoLabel) {
+  var newTodo = {
+    label: newTodoLabel,
+    priority:1,
+    done: false
+  };
+  this.todos.push(newTodo);
+}
+deleteTodo(todo) {
+  this.todos = this.todos.filter(t => t.label !== todo.label );
+}
 }
